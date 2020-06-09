593850f66db5638536912f00f519166227c09e2b
Richard Barbsosa <richardbarbosa017@gmail.com>
2020-06-09 20:45:59 -0300

    Bug fix e Drivenforms criados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,72 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatDialogModule} from '@angular/material/dialog';
 import { AppComponent } from './app.component';
 
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import{ MatListModule} from '@angular/material/list';
+import{ MatGridListModule} from '@angular/material/grid-list';
+
+import{MatCardModule} from '@angular/material/card';
+import{MatButtonModule} from '@angular/material/button';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
+import {DishService} from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import {PromotionService} from'./services/promotion.service';
+import {LeaderService} from './services/leader.service';
+import { LoginComponent } from './login/login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatListModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    DishService,
+    PromotionService,
+    LeaderService
+  ],
+  entryComponents:[
+    LoginComponent,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/leader.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { LEADERS } from '../shared/leaders';
+import { Leader } from '../shared/leaders';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LeaderService {
+
+  constructor() { }
+
+  getLeaders(): Leader[]{
+    return LEADERS;
+  }
+  getFeaturedLeader(): Leader {
+    return LEADERS.filter((leader) => leader.featured)[0];
+  }
+}
