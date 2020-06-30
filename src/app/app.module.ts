ee982e20a54852e31d0c289030082a205a57fc25
Richard Barbsosa <richardbarbosa017@gmail.com>
2020-06-29 22:01:41 -0300

    Directive Highlight

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,6 +36,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
 import {MatSliderModule} from '@angular/material/slider';
 import{ ProcessHTTPMsgService } from './services/process-httpmsg.service';
+import { HighlightDirective } from './directives/highlight.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -46,7 +47,8 @@ import{ ProcessHTTPMsgService } from './services/process-httpmsg.service';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
